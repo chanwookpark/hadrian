20d7afb777425b1f53f7cbeb625352fd648ea54b
chanwook <chanwook.god@gmail.com>
2014-08-04 12:37:30 +0900

    개발 중.. 임시로 빌드를 위해서 커밋 함

---
+++ src/main/java/hadrian/meta/AccessToken.java
@@ -0,0 +1,12 @@
+package hadrian.meta;
+
+import java.lang.annotation.*;
+
+/**
+ * Created by chanwook on 2014. 7. 28..
+ */
+@Target(ElementType.PARAMETER)
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
+public @interface AccessToken {
+}
